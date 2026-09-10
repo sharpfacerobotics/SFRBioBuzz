72c1315b6db5010ea738e2d42b7318f892a7c5a2
Orange X <alex.xu202208@gmail.com>
2026-09-10 08:00:17 -0700

    Update Tuning.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuning.java
@@ -2,4 +2,14 @@ package org.firstinspires.ftc.teamcode.pedro;
 
 public class Tuning {
     // Tuners go here
+    @Tuner
+    public static Procedure mecanumTuner() {
+        return new MecanumTuner();
+    }
+    //BEFORE RUNNING THE BELOW CODE, CONTINUE FROM Open Autotune at https://pedropathing.com/docs/pathing/tuning/drivetrain/mecanum. 
+    //*Ensure that you first build the code and upload it to the robot
+    @Tuner
+    public static Procedure tests() {
+        return new Tests(hardwareMap -> new Mecanum(hardwareMap, Constants.drivetrainConfig), null, null);
+    }
 }
